cfeb2d18b6ad7d91a56b5252b281d65d263ef618
Alistair Kearney <hi@alistairkearney.com>
2016-04-27 12:46:51 +1000

    Checking for devkits before attempting to render the page as JSON. Fixes #1

--- src/Includes/JsonRendererLauncher.php
+++ src/Includes/JsonRendererLauncher.php
@@ -20,15 +20,39 @@ function renderer_json($mode){
     cleanup_session_cookies();
 
     if(in_array('JSON', Frontend::Page()->pageData()['type'])) {
+
         // Load the output into a SimpleXML Container and convert to JSON
-        $xml = new SimpleXMLElement($output, LIBXML_NOCDATA);
+        try{
+          $xml = new SimpleXMLElement($output, LIBXML_NOCDATA);
 
-        // Convert the XML to a plain array. This step is necessary as we cannot
-        // use JSON_PRETTY_PRINT directly on a SimpleXMLElement object
-        $outputArray = json_decode(json_encode($xml));
+          // Convert the XML to a plain array. This step is necessary as we cannot
+          // use JSON_PRETTY_PRINT directly on a SimpleXMLElement object
+          $outputArray = json_decode(json_encode($xml));
 
-        // Now put the array through a json_encode
-        $output = json_encode($outputArray, JSON_PRETTY_PRINT);
+          // Now put the array through a json_encode
+          $output = json_encode($outputArray, JSON_PRETTY_PRINT);
+
+        } catch(\Exception $e) {
+            // This happened because the input was not valid XML. This could
+            // occur for a few reasons, but there are two scenarios
+            // we are interested in.
+
+            // 1) This is a devkit page (profile, debug etc). We want the data
+            //    to be passed through and displayed rather than converted into
+            //    JSON. There is no easy way in Symphony to tell if a devkit has
+            //    control over the page, so instead lets inspect the output for
+            //    any signs a devkit is rendering the page.
+
+            // 2) It is actually bad XML. In that case we need to let the error
+            //    bubble through.
+
+            // Currently the easiest method is to check for the devkit.min.css
+            // in the output. This may fail in the furture if this file is
+            // renamed or moved.
+            if(!preg_match("@\/symphony\/assets\/css\/devkit.min.css@", $output)) {
+              throw $e;
+            }
+        }
     }
 
     echo $output;
