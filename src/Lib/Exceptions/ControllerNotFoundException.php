fe569760e16adcf7c7dbd6b11737573a0fe09e13
Alistair Kearney <hi@alistairkearney.com>
2016-06-17 09:56:16 +1000

    Updated ControllerNotFoundException and MethodNotAllowedException to extend the new AbstractApiException class

--- src/Lib/Exceptions/ControllerNotFoundException.php
+++ src/Lib/Exceptions/ControllerNotFoundException.php
@@ -2,6 +2,12 @@
 
 namespace Symphony\ApiFramework\Lib\Exceptions;
 
-class ControllerNotFoundException extends \Exception
+use Symphony\ApiFramework\Lib;
+use Symfony\Component\HttpFoundation\Response;
+
+class ControllerNotFoundException extends Lib\AbstractApiException
 {
+    public function __construct($controller, $code = 0, Exception $previous = null) {
+        parent::__construct(Response::HTTP_NOT_FOUND, "Controller '{$controller}' could not be located.", $code, $previous);
+    }
 }
---
+++ src/Lib/Exceptions/MethodNotAllowedException.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Symphony\ApiFramework\Lib\Exceptions;
+
+use Symphony\ApiFramework\Lib;
+use Symfony\Component\HttpFoundation\Response;
+
+class MethodNotAllowedException extends Lib\AbstractApiException
+{
+    public $method;
+
+    public function getHttpMethod() {
+        return $this->method;
+    }
+
+    public function __construct($method, $code = 0, Exception $previous = null) {
+        // Save the method
+        $this->method = $method;
+
+        parent::__construct(Response::HTTP_METHOD_NOT_ALLOWED, "Method '{$this->method}' is not allowed by this endpoint.", $code, $previous);
+    }
+}
