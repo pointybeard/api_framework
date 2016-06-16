f96fd85501a3e56a4747d81d1b36ed2578dd9189
Alistair Kearney <hi@alistairkearney.com>
2016-06-17 09:54:47 +1000

    Added new abstract extension 'AbstractApiException' which is used by ControllerNotFoundException and MethodNotAllowedException. Allows setting of HTTP response code to avoid every exception thrown appearing as a '500 Internal Server Error'

---
+++ src/Lib/AbstractApiException.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Symphony\ApiFramework\Lib;
+
+class AbstractApiException extends \Exception
+{
+    private $status;
+
+    public function getHttpStatusCode() {
+        return $this->status;
+    }
+
+    public function __construct($status, $message, $code = 0, Exception $previous = null) {
+        // Save the status code
+        $this->status = $status;
+
+        parent::__construct($message, $code, $previous);
+    }
+}
