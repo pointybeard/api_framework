d976efc316aa4400c3f14d928a0ce7aa628dae41
Alistair Kearney <hi@alistairkearney.com>
2017-09-26 18:13:05 +1000

    The recurive method recursiveApplyTransformationToArray() had a logic bug that meant the result from lower levels was not propagated up the chain. Addiitonally, the @attributes array was not being dealt with consistently. The last transformer to run will trigger a cleanup which removes the @attributes array. Fixes #15 #16 #17

--- src/Lib/Transformer.php
+++ src/Lib/Transformer.php
@@ -37,17 +37,28 @@ class Transformer
   }
 
   public function run(array $input) {
-    foreach ($this->transformations as $t) {
-      $input = $this->recursiveApplyTransformationToArray($input, $t);
-    }
+    $totalTransformantionCount = count($this->transformations);
+    $preserveAttributes = true;
 
+    foreach ($this->transformations as $index => $t) {
+      // #17 - Check if there are any more transformers remaining. If no, we can
+      // trigger a cleanup by setting $preserveAttributes to false. This will
+      // remove all @attribute arrays during the next recursion.
+      if($index >= $totalTransformantionCount - 1) {
+          $preserveAttributes = false;
+      }
+
+      // Run the transformation.
+      $input = $this->recursiveApplyTransformationToArray($input, $t, $preserveAttributes);
+    }
     return $input;
   }
 
-  private function recursiveApplyTransformationToArray(array $input, Transformation $transformation) {
+  private function recursiveApplyTransformationToArray(array $input, Transformation $transformation, $preserveAttributes = true) {
     $result = [];
 
     $attributes = isset($input['@attributes']) ? $input['@attributes'] : [];
+
     // Strip out the attributes.
     unset($input['@attributes']);
 
@@ -64,13 +75,24 @@ class Transformer
     foreach($input as $key => $value) {
         $next = (is_array($value)
             // It's an array, so go deeper.
-            ? $this->recursiveApplyTransformationToArray($value, $transformation)
+            ? $this->recursiveApplyTransformationToArray($value, $transformation, $preserveAttributes)
             // Non-array, just append it back in and return
             : $value
         );
+
         // Preserve array indexes
-        $isAssoc ? $result[$key] = $next : array_push($result, $value);
+        // #16 - Using $next instead of $value.
+        $isAssoc ? $result[$key] = $next : array_push($result, $next);
     }
+
+    // #15 & #17 - Unless it's an empty array or we deliberately ask for it to be
+    // left out, put attributes back. They may be required by subsequent
+    // transformers. The last transformer in the chain will trigger a cleanup
+    // anyway.
+    if($preserveAttributes == true && !empty($attributes)) {
+        $result["@attributes"] = $attributes;
+    }
+
     return $result;
   }
 }
