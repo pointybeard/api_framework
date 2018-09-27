5dd649614b6539c42fccd98305f799e18e41534a
Alistair Kearney <hi@alistairkearney.com>
2018-09-27 10:08:00 +1000

    Fixed Exception namespace in __construct() function signature (Fixes #19).

--- src/Lib/AbstractApiException.php
+++ src/Lib/AbstractApiException.php
@@ -10,7 +10,7 @@ abstract class AbstractApiException extends \Exception
         return $this->status;
     }
 
-    public function __construct($status, $message, $code = 0, Exception $previous = null) {
+    public function __construct($status, $message, $code = 0, \Exception $previous = null) {
         // Save the status code
         $this->status = $status;
 
