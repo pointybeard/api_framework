888f64896d78b9c22d48e470adbf5913caf5b67f
Alannah Kearney <hi@alannahkearney.com>
2018-11-02 15:59:32 +1000

    Adding HTTP request query string to cache. Changed header X-API-Framework-Cache to hit or miss rather than yes or no.

--- src/Lib/CacheableJsonFrontendPage.php
+++ src/Lib/CacheableJsonFrontendPage.php
@@ -8,12 +8,30 @@ class CacheableJsonFrontendPage extends JsonFrontendPage
 {
     public function generate($page = null)
     {
+
+        // Grab any query string values. We'll store this with the
+        // cache to improve hit accuracy.
+        $query = [];
+        parse_str($_SERVER['QUERY_STRING'], $query);
+        asort($query, SORT_NATURAL | SORT_FLAG_CASE);
+
+        // symphony-page is always set, otherwise we wouldn't be here. Get rid
+        // of it to make cache entry slightly less verbose.
+        unset($query['symphony-page']);
+
+        $orderedQueryString = "";
+        foreach ($query as $key => $value) {
+            $orderedQueryString .= "&{$key}={$value}";
+        }
+        $orderedQueryString = trim($orderedQueryString, '&');
+
         // Logic for checking if there is cached page data
-        $cache = Models\PageCache::loadFromPage($page);
+        $cache = Models\PageCache::loadCurrentFromPageAndQueryString($page, $orderedQueryString);
+
         if ($cache instanceof Models\PageCache) {
             JsonFrontend::Page()->addHeaderToPage(
-                'X-API-Framework-Cached',
-                'yes'
+                'X-API-Framework-Cache',
+                'hit'
             );
             return $cache->render();
         }
@@ -30,15 +48,16 @@ class CacheableJsonFrontendPage extends JsonFrontendPage
         (new Models\PageCache)
             ->headers(json_encode($headers, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES))
             ->contents($output)
+            ->queryString($orderedQueryString)
             ->dateCreatedAt('now')
-            ->dateExpiresAt('+1 hour')
+            ->dateExpiresAt(date(DATE_RFC2822, strtotime("+1 hour")))
             ->page($page)
             ->save()
         ;
 
         JsonFrontend::Page()->addHeaderToPage(
-            'X-API-Framework-Cached',
-            'no'
+            'X-API-Framework-Cache',
+            'miss'
         );
 
         return $output;
