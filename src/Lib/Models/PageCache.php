41cde2403472114726964f542193ee27f1c227c4
Alannah Kearney <hi@alannahkearney.com>
2018-11-02 16:00:13 +1000

    Added loadCurrentFromPageAndQueryString(), hasExpired(), and expire() methods to PageCache model.

--- src/Lib/Models/PageCache.php
+++ src/Lib/Models/PageCache.php
@@ -27,6 +27,46 @@ final class PageCache extends ClassMapper\AbstractClassMapper
         ];
     }
 
+    public static function loadCurrentFromPageAndQueryString($page, $queryString)
+    {
+        self::findSectionFields();
+
+        $queryStringSQL = "%2\$s.value = :query_string";
+        if (empty($queryString)) {
+            $queryStringSQL = "(%2\$s.value IS NULL OR %2\$s.value = '')";
+        }
+
+        $db = SymphonyPDO\Loader::instance();
+        $query = $db->prepare(self::fetchSQL(sprintf(
+            " %1\$s.value = :page AND {$queryStringSQL} AND %3\$s.date > NOW()",
+            self::findJoinTableFieldName('page'),
+            self::findJoinTableFieldName('query-string'),
+            self::findJoinTableFieldName('expires-at')
+        )));
+
+        $query->bindValue(':page', $page, \PDO::PARAM_STR);
+
+        if (!empty($queryString)) {
+            $query->bindValue(':query_string', $queryString, \PDO::PARAM_STR);
+        }
+
+        $query->execute();
+
+        $result = (new SymphonyPDO\Lib\ResultIterator(__CLASS__, $query));
+
+        if ($result->count() > 1) {
+            // Multiple valid cache entries. Delete them all and let a new cache
+            // entry be produced.
+            foreach ($result as $r) {
+                $r->delete();
+            }
+
+            return false;
+        }
+
+        return $result->current();
+    }
+
     public function render()
     {
         // Headers
@@ -61,5 +101,18 @@ final class PageCache extends ClassMapper\AbstractClassMapper
         $query->execute();
 
         return (new SymphonyPDO\Lib\ResultIterator(__CLASS__, $query))->current();
+    }
+
+    public function expire()
+    {
+        return $this
+            ->dateExpiresAt("now")
+            ->save()
+        ;
+    }
+
+    public function hasExpired()
+    {
+        return (bool)(strtotime($this->dateExpiresAt) < time());
     }
 }
