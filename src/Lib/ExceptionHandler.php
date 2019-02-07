2c4b6c569e1ef700ca497bd026a50852a4ccfd18
Alannah Kearney <hi@alannahkearney.com>
2019-02-07 18:13:27 +1100

    ExceptionHandler now adds the XML context when rendering page fails due to XSLT error.

--- src/Lib/ExceptionHandler.php
+++ src/Lib/ExceptionHandler.php
@@ -63,7 +63,6 @@ class ExceptionHandler extends GenericExceptionHandler
                 (isset($query['execution_time']) ? $query['execution_time'] : null)
             );
         }
-
         return $result;
     }
 
@@ -98,14 +97,54 @@ class ExceptionHandler extends GenericExceptionHandler
             $output['debug'] = [
                 'file' => $e->getFile(),
                 'line' => $e->getLine(),
-                'severity' => ($e instanceof Lib\ErrorException ? \GenericErrorHandler::$errorTypeStrings[$e->getSeverity()] : 'Fatal Error'),
-                'code_trace' => (count($e->getTrace()) > 0 ? self::getCodeTrace($e->getTrace()) : null),
+                'severity' => (
+                    $e instanceof Lib\ErrorException
+                        ? \GenericErrorHandler::$errorTypeStrings[$e->getSeverity()]
+                        : 'Fatal Error'
+                ),
+                'code_trace' => (
+                    count($e->getTrace()) > 0
+                        ? self::getCodeTrace($e->getTrace())
+                        : null
+                ),
                 'database_trace' => self::getDatabaseTrace($databaseDebug),
             ];
+
+            // See of this is an XSLT error. If so, include the XML output
+            // since that is what is most likely failing.
+            if (
+                $e instanceof \SymphonyErrorPage &&
+                $e->getTemplateName() == 'xslt' &&
+                isset($e->getAdditional()->proc)
+            ) {
+                $err = $e->getAdditional()->proc->getError(false, true);
+                if (isset($err['value']) && isset($err['value']['context'])) {
+                    $xml = $err['value']['context'];
+
+                    // Convert double quotes into single quotes. This stops
+                    // JSON from escaping double quotes
+                    $xml = preg_replace("@\"@", "'", $xml);
+
+                    // Convert tab characters into 2 spaces. We cannot display
+                    // control characters in JSON output.
+                    $xml = preg_replace("@\t@", "  ", $xml);
+
+                    // Split the XML into individual lines. This is because
+                    // any newline control characters will be escaped
+                    $xmlLines = preg_split("@[\r\n]@", $xml);
+
+                    // Add each line of the XML to the output. Use a zero padded
+                    // line number for readablity.
+                    $output['debug']['context'] = [];
+                    foreach ($xmlLines as $n => $l) {
+                        $output['debug']['context'][sprintf('%04d', $n+1)] = $l;
+                    }
+                }
+            }
         }
 
         // output and die
-        echo json_encode($output, JsonFrontend::DEFAULT_ENCODING_OPTIONS);
+        echo json_encode($output, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
         exit(255);
     }
 }
