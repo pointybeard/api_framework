395dee08608d0d8e9baa936027cae7d4edb5b1f0
Alannah Kearney <hi@alannahkearney.com>
2019-05-16 10:36:01 +0000

    Moved cache command from bin/ to commands/

--- commands/cache.php
+++ commands/cache.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Symphony\Console\Command\Api_Framework;
+namespace Symphony\Console\Commands\Api_Framework;
 
 use SymphonyPDO;
 use Extension_API_Framework;
